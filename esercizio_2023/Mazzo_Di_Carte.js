bdf9abb601d85fb006acb99e3f4c4a9c3de5954d
Netta90 <58398245+Netta90@users.noreply.github.com>
2023-02-04 11:58:09 +0100

    Nuovo esercizio mischia carte

---
+++ esercizio_2023/Mazzo_Di_Carte.js
@@ -0,0 +1,28 @@
+/*Utilizzando la programmazione ad oggetti create una classe 
+per il mazzo e tutti i metodi necessari per mischiarlo, pescare e rimettere una carta in fondo. 
+Limitatevi all’inizio ad avere 13 carte, quindi dall’asso al re.
+Utilizzate dei console log per mandare a schermo la situazione del mazzo ad ogni operazione nonché le carte che pescate e rimettete nel mazzo.
+https://lorenzoneri.com/come-mescolare-shuffle-un-array-in-javascript/ */
+
+
+class MazzoDiCarte {
+    constructor(arrayDiCarte){
+
+    }
+
+    mischiaCarte() {
+
+    }
+
+    pescaCarte() {
+
+    }
+
+
+    rmettereCartaInfondo(){
+
+    }
+}
+
+
+
